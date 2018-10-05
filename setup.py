d8f40b6a5d85622cfa26e492fc916c6177f71b85
Antoine Pietri <antoine.pietri1@gmail.com>
2018-10-05 12:56:50 +0200

    Add requirements-test.txt

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 
+import os
 from setuptools import setup, find_packages
 
 
-def parse_requirements():
+def parse_requirements(name=None):
+    if name:
+        reqf = 'requirements-%s.txt' % name
+    else:
+        reqf = 'requirements.txt'
+
     requirements = []
-    for reqf in ('requirements.txt', 'requirements-swh.txt'):
-        with open(reqf) as f:
-            for line in f.readlines():
-                line = line.strip()
-                if not line or line.startswith('#'):
-                    continue
-                requirements.append(line)
+    if not os.path.exists(reqf):
+        return requirements
+
+    with open(reqf) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            requirements.append(line)
     return requirements
 
 
@@ -22,8 +30,9 @@ setup(
     author_email='swh-devel@inria.fr',
     url='https://forge.softwareheritage.org/diffusion/DVAU/',
     packages=find_packages(),
-    install_requires=parse_requirements(),
+    install_requires=parse_requirements() + parse_requirements('swh'),
     setup_requires=['vcversioner'],
+    extras_require={'testing': parse_requirements('test')},
     vcversioner={},
     include_package_data=True,
     zip_safe=False,

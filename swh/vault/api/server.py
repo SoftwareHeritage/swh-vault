9d146a5c0180c7682c7547393155aca7021d7bd2
Antoine Lambert <antoine.lambert@inria.fr>
2019-05-07 14:38:50 +0200

    api.server: Move config parsing in make_app_from_configfile

--- swh/vault/api/server.py
+++ swh/vault/api/server.py
@@ -199,12 +199,7 @@ def make_app(backend, **kwargs):
     return app
 
 
-def get_local_backend(config_file):
-    if os.path.isfile(config_file):
-        cfg = config.read(config_file, DEFAULT_CONFIG)
-    else:
-        cfg = config.load_named_config(config_file, DEFAULT_CONFIG)
-
+def get_local_backend(cfg):
     if 'vault' not in cfg:
         raise ValueError("missing '%vault' configuration")
 
@@ -231,7 +226,11 @@ def get_local_backend(config_file):
 
 def make_app_from_configfile(config_file=DEFAULT_CONFIG_PATH, **kwargs):
     config_file = os.environ.get('SWH_CONFIG_FILENAME', config_file)
-    vault = get_local_backend(config_file)
+    if os.path.isfile(config_file):
+        cfg = config.read(config_file, DEFAULT_CONFIG)
+    else:
+        cfg = config.load_named_config(config_file, DEFAULT_CONFIG)
+    vault = get_local_backend(cfg)
     return make_app(backend=vault, **kwargs)
 
 
